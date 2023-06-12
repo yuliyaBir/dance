e913e85c444f3877d39d6ca6e650ba1b49078f4a
yuliyaBir <zhivo4ka@gmail.com>
2023-06-12 09:41:21 +0200

    Initial commit

---
+++ src/main/java/be/vdab/dance/DanceApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.dance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceApplication.class, args);
+	}
+
+}
